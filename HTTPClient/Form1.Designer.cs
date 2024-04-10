16f1a4e4e08f0d32c6909c0237d20fa092b4ec01
Diephho <okdiep1@gmail.com>
2024-04-10 17:36:20 +0700

    First

---
+++ HTTPClient/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace HTTPClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.postData = new System.Windows.Forms.TextBox();
+            this.cookieBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.STT = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Type = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Value = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(25, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(575, 30);
+            this.textBox1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "GET",
+            "POST",
+            "PUT",
+            "DELETE"});
+            this.comboBox1.Location = new System.Drawing.Point(618, 32);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 28);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(772, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 35);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(25, 185);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(967, 533);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // postData
+            // 
+            this.postData.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.postData.Location = new System.Drawing.Point(25, 80);
+            this.postData.Name = "postData";
+            this.postData.Size = new System.Drawing.Size(575, 30);
+            this.postData.TabIndex = 4;
+            // 
+            // cookieBox
+            // 
+            this.cookieBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cookieBox.Location = new System.Drawing.Point(105, 135);
+            this.cookieBox.Name = "cookieBox";
+            this.cookieBox.Size = new System.Drawing.Size(575, 30);
+            this.cookieBox.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 142);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "cookie:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.STT,
+            this.Type,
+            this.Value});
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(1030, 97);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(736, 682);
+            this.listView1.TabIndex = 7;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // STT
+            // 
+            this.STT.Text = "STT";
+            // 
+            // Type
+            // 
+            this.Type.Text = "Type";
+            this.Type.Width = 250;
+            // 
+            // Value
+            // 
+            this.Value.Text = "Value";
+            this.Value.Width = 400;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1778, 791);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cookieBox);
+            this.Controls.Add(this.postData);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox postData;
+        private System.Windows.Forms.TextBox cookieBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader STT;
+        private System.Windows.Forms.ColumnHeader Type;
+        private System.Windows.Forms.ColumnHeader Value;
+    }
+}
+
---
+++ HTTPClient/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Diagnostics;
+using System.Net.Http.Headers;
+namespace HTTPClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void ShowHeaders(HttpResponseHeaders headers)
+        {
+            if (headers == null)
+            {
+                // Headers is null, handle this case if needed
+                return;
+            }
+
+            // Clear existing items in listView1
+            listView1.Items.Clear();
+
+            int numSTT = 0;
+            foreach (KeyValuePair<string, IEnumerable<string>> header in headers)
+            {
+                ListViewItem headerList = new ListViewItem();
+                headerList.Text = numSTT.ToString();
+                headerList.SubItems.Add(header.Key);
+
+                // Check if the value is null
+                if (header.Value != null)
+                {
+                    // Process the header values
+                    List<string> values = header.Value.ToList();
+
+                    if (values.Any())
+                    {
+                        // Join values into a single string
+                        headerList.SubItems.Add(string.Join(",", values));
+                    }
+                    else
+                    {
+                        // If the value list is empty, just add an empty string
+                        headerList.SubItems.Add("");
+                    }
+                }
+                else
+                {
+                    // If the value is null, add an empty string
+                    headerList.SubItems.Add("");
+                }
+
+                numSTT++;
+                listView1.Items.Add(headerList);
+            }
+        }
+
+        private async Task<string> GetHTMLAsync(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                //client.DefaultRequestHeaders.Add("Content-Type", "application/");
+
+                HttpResponseMessage response = await client.GetAsync(url);
+
+                ShowHeaders(response.Headers);
+              
+                if (response.IsSuccessStatusCode)
+                {
+                    // Đọc nội dung với Encoding.UTF8 để đảm bảo đọc đúng bộ mã.
+                    string responseData = await response.Content.ReadAsStringAsync();
+                    return responseData;
+                }
+                else
+                {
+                    return "Error: " + response.StatusCode;
+                }
+            }
+        }
+
+        private async Task<string> GetDataWithCookieAsync(string url, string cookie)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, url);
+
+                // Thêm tiêu đề User-Agent vào yêu cầu để định danh trình duyệt khi gửi yêu cầu.
+                request.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36");
+
+                // Phân tách cookie thành các cặp key-value và thêm chúng vào CookieContainer.
+                var cookiePairs = cookie.Split(';');
+                foreach (var pair in cookiePairs)
+                {
+                    var cookieKeyValue = pair.Split('=');
+                    if (cookieKeyValue.Length == 2)
+                    {
+                        request.Headers.Add("Cookie", $"{cookieKeyValue[0].Trim()}={cookieKeyValue[1].Trim()}");
+                    }
+                }
+
+                HttpResponseMessage response = await client.SendAsync(request);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseData = await response.Content.ReadAsStringAsync();
+                    return responseData;
+                }
+                else
+                {
+                    return "Error: " + response.StatusCode;
+                }
+            }
+        }
+
+
+        private async Task<string> PostDataAsync(string szUrl, string postData)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                // Tạo nội dung POST từ dữ liệu đã cung cấp
+                HttpContent content = new StringContent(postData, Encoding.UTF8, "application/x-www-form-urlencoded");
+
+                // Gửi yêu cầu POST đến URL đã chỉ định và nhận phản hồi
+                HttpResponseMessage response = await client.PostAsync(szUrl, content);
+
+                // Đảm bảo yêu cầu thành công
+                if (response.IsSuccessStatusCode)
+                {
+                    // Đọc nội dung phản hồi và trả về dưới dạng chuỗi
+                    string responseData = await response.Content.ReadAsStringAsync();
+                    return responseData;
+                }
+                else
+                {
+                    // Xử lý trường hợp lỗi nếu cần thiết
+                    return "Error: " + response.StatusCode;
+                }
+            }
+        }
+        private async Task<string> DeleteDataAsync(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.DeleteAsync(url);
+
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string noti = "DELETE request successful.";
+                    return noti;
+                }
+                else
+                {
+                    return $"Error: {response.StatusCode}";
+                }
+            }
+        }
+        static async Task<string> PutDataAsync(string url, string data)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await client.PutAsync(url, content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    // Đọc nội dung phản hồi và trả về dưới dạng chuỗi
+                    string responseData = await response.Content.ReadAsStringAsync();
+                    return responseData;
+                }
+                else
+                {
+                    return $"Error: {response.StatusCode}";
+                }
+            }
+        }
+        
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            switch (comboBox1.Text)
+            {
+                case "GET":
+                {
+                    if (cookieBox.Text == "")
+                    {
+                        string responseData = await GetHTMLAsync(textBox1.Text);
+                        richTextBox1.Text = responseData;
+                    }
+                    else
+                    {
+                        string responseData = await GetDataWithCookieAsync(textBox1.Text, cookieBox.Text);
+                        richTextBox1 .Text = responseData;
+                    }
+                    break;
+                }
+                    
+                case "POST":
+                {
+                    string responseData = await PostDataAsync(textBox1.Text, postData.Text);
+                    richTextBox1.Text = responseData;
+                    break;
+                }
+                case "DELETE":
+                {
+                    string noti = await DeleteDataAsync(textBox1.Text);
+                    richTextBox1.Text = noti;
+                    break;
+                }
+                case "PUT":
+                {
+                    string noti = await PutDataAsync(textBox1.Text, postData.Text);
+                    richTextBox1.Text = noti;
+                    break;
+                }
+                default:
+                {
+                    MessageBox.Show("Invalid method");
+                    break;
+                }
+            }
+            File.WriteAllText("res.html", richTextBox1.Text);
+            Process.Start("res.html");
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
